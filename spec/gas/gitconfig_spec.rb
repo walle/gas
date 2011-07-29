1f55080f0dbba139ea437f08b03f679c82fdc9ad
Fredrik Wallgren <fredrik.wallgren@gmail.com>
2011-07-30 01:07:50 +0200

    Refactor the mocking of ` to method to make test code a lot clearer

--- spec/gas/gitconfig_spec.rb
+++ spec/gas/gitconfig_spec.rb
@@ -11,9 +11,8 @@ describe Gas::Gitconfig do
   end
 
   it 'should be able to get current user from gitconfig' do
-    # To be able to mock ` (http://blog.astrails.com/2010/7/5/how-to-mock-backticks-operator-in-your-test-specs-using-rr)
-    mock(@gitconfig).__double_definition_create__.call(:`, 'git config --global --get user.name') { @name }
-    mock(@gitconfig).__double_definition_create__.call(:`, 'git config --global --get user.email') { @email }
+    mock_cli_call(@gitconfig, 'git config --global --get user.name') { @name }
+    mock_cli_call(@gitconfig, 'git config --global --get user.email') { @email }
 
     user = @gitconfig.current_user
     user.name.should == @name
@@ -21,8 +20,8 @@ describe Gas::Gitconfig do
   end
 
   it 'should return nil if no current user is present in gitconfig' do
-    mock(@gitconfig).__double_definition_create__.call(:`, 'git config --global --get user.name') { nil }
-    mock(@gitconfig).__double_definition_create__.call(:`, 'git config --global --get user.email') { nil }
+    mock_cli_call(@gitconfig, 'git config --global --get user.name') { nil }
+    mock_cli_call(@gitconfig, 'git config --global --get user.email') { nil }
 
     @gitconfig.current_user.should == nil
   end
@@ -31,10 +30,10 @@ describe Gas::Gitconfig do
     name = 'Test Testsson'
     email = 'test@testsson.com'
 
-    mock(@gitconfig).__double_definition_create__.call(:`, "git config --global user.name #{name}") { nil }
-    mock(@gitconfig).__double_definition_create__.call(:`, "git config --global user.email #{email}") { nil }
-    mock(@gitconfig).__double_definition_create__.call(:`, 'git config --global --get user.name') { name }
-    mock(@gitconfig).__double_definition_create__.call(:`, 'git config --global --get user.email') { email }
+    mock_cli_call(@gitconfig, "git config --global user.name #{name}") { nil }
+    mock_cli_call(@gitconfig, "git config --global user.email #{email}") { nil }
+    mock_cli_call(@gitconfig, 'git config --global --get user.name') { name }
+    mock_cli_call(@gitconfig, 'git config --global --get user.email') { email }
 
     @gitconfig.change_user name, email
 
