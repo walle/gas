90b6baefb6028e080903692d925423e8758eddd1
TheNotary <no.email@plz.com>
2012-07-24 12:58:59 -0600

    added VCR and depenedencies, and got a test working with it.

--- spec/integration/ssh_spec.rb
+++ spec/integration/ssh_spec.rb
@@ -208,7 +208,7 @@ describe Gas::Ssh do
       @sample_rsa = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQDn74QR9yHb+hcid8iH3+FTaEwnKtwjttseJDbIA2PaivN2uvESrvHlp8Ss/cRox3fFu34QR5DpdOhlfULjTX7yKVuxhaNrAJaqg8rX8hgr9U1Botnyy1DBueEyyA3o1fxRkmwTf6FNnkt1BxWP635tD0lbmUubwaadXjQqPOf3Uw=="
 
 
-      Gas::Ssh.stub!(:get_username_and_password_and_authenticate).and_return(@credentials)  #TODO: remove this obsolete stub when it's really obsolete
+      Gas::Ssh.stub!(:get_username_and_password_and_authenticate).and_return(@credentials)
       #Gas::GithubSpeaker.stub!(:get_username_and_password_and_authenticate).and_return({:account_name => @username, :password => @password})
       #Gas::Ssh::GithubSpeaker.stub!(:get_username_and_password_and_authenticate).and_return({:account_name => @username, :password => @password})
     end
